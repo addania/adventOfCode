deabe434e5ad07e70cd38d63ebc8847393ed2ae4
addania <mirka.schw@gmail.com>
2019-12-05 00:13:34 +0100

    adding day2 2019

--- src/components/codeAdvent20190201.js
+++ src/components/codeAdvent20190201.js
@@ -7,21 +7,48 @@ export const CodeAdvent20190201 = () => {
   const [result, setResult] = useState();
 
   function handleClick() {
-    let output=x(input);
-    setResult(output);
+    let replacedInput=replace(input);
+    let output= calc (replacedInput);
+    setResult(output[0]);
   }
 
   return (
     <div>
-      <p style={{ fontWeight: "bold" }}>NAME:</p>
-      <p>XXX: {result} </p>
-      <button style={{ backgroundColor: "#6BAA75" }} onClick={handleClick}><span onClick={handleClick}>XXX</span>
+      <p style={{ fontWeight: "bold" }}>Program Alarm:</p>
+      <p>Program halts at position: {result} </p>
+      <button style={{ backgroundColor: "#6BAA75" }} onClick={handleClick}><span onClick={handleClick}>Get Position</span>
       </button>      
     </div>
   );
 };
 
-function x(input){
-  
-  return 0;
+const replace = (input)=> {
+ input[1]=12;
+ input[2]=2;
+ console.log(input);
+ return input;
+}
+
+
+const calc = (input)=> {
+console.log("len",input.length);
+for (let i=0;i<input.length;i+=4){
+ console.log("i is",i);
+if (input[i]==99){
+ console.log("condition 99 is triggered");
+break;
+}else if(input[i]==1){
+ console.log("condition 1 is triggered");
+let newValue=input[input[i+1]]+input[input[i+2]];
+console.log("val1", input[i+1], "val2",input[i+2], "new value", newValue);
+input[input[i+3]]=newValue;
+}else if(input[i]==2){
+ console.log("condition 2 is triggered");
+let newValue2=input[input[i+1]]*input[input[i+2]];
+input[input[i+3]]=newValue2;
+} else{
+ console.log("nothing is triggered");
+}
+}
+return input;
 }
--- src/components/codeAdvent20190202.js
+++ src/components/codeAdvent20190202.js
@@ -7,21 +7,66 @@ export const CodeAdvent20190202 = () => {
   const [result, setResult] = useState();
 
   function handleClick() {
-    let output=x(input);
+    let output = final(input);
     setResult(output);
   }
 
   return (
     <div>
-      <p style={{ fontWeight: "bold" }}>NAME:</p>
-      <p>XXX: {result} </p>
-      <button style={{ backgroundColor: "#6BAA75" }} onClick={handleClick}><span onClick={handleClick}>XXX</span>
-      </button>      
+      <p style={{ fontWeight: "bold" }}>Program Alarm:</p>
+      <p> Program halts at position: {result} </p>
+      <button style={{ backgroundColor: "#6BAA75" }} onClick={handleClick}><span onClick={handleClick}>Get Position</span>
+      </button>
     </div>
-  );
+    );
 };
 
-function x(input){
-  
-  return 0;
+function final(input){
+  let output=[];
+  let finalNumber=0;
+  for (let x=0;x<100;x++){
+   for (let y=0;y<100;y++){
+    
+     let newInput=[...input]
+     
+     let replacedInput=replace(newInput, x, y);
+
+     let output=calc(replacedInput);
+     
+     let first=output[0];
+     if (first==19690720){
+      finalNumber=100*x+y;
+     };
+}
+}
+return finalNumber;
+}
+
+const replace = (input, first, second)=> {
+input[1]=first;
+input[2]=second;
+return input;
+}
+
+const calc = (input)=> {
+
+for (let i=0;i<input.length;i+=4){
+
+if (input[i]==99){
+
+i=input.length;
+}else if(input[i]==1){
+
+let newValue=input[input[i+1]]+input[input[i+2]];
+
+input[input[i+3]]=newValue;
+}else if(input[i]==2){
+
+let newValue2=input[input[i+1]]*input[input[i+2]];
+input[input[i+3]]=newValue2;
+} else{
+
+}
+}
+return input;
 }
