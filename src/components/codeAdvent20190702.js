fc900467ff05300d8385a1c124be8a65432044f9
addania <mirka.schw@gmail.com>
2019-12-31 20:41:46 +0100

    progress on 2019 day 7 part 2

--- src/components/codeAdvent20190702.js
+++ src/components/codeAdvent20190702.js
@@ -3,7 +3,12 @@ import data from "./AdventCodeInputs/CodeAdvent201907.json";
 import "./component.css";
 
 export const CodeAdvent20190702 = () => {
-  const [amplifierControllerSoftware, setAmplifierControllerSoftware] = useState([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]);
+  //const [amplifierControllerSoftware, setAmplifierControllerSoftware] = useState([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]);
+  const [amplifierControllerSoftware, setAmplifierControllerSoftware] = useState([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]);
+  
+
   const [maxSetting, setMaxSetting] = useState();
   const [result, setResult] = useState();
 
@@ -14,11 +19,17 @@ export const CodeAdvent20190702 = () => {
 
    
 
-   for (let am1=9;am1<10;am1++){
+  /* for (let am1=9;am1<10;am1++){
    	for (let am2=8;am2<9;am2++){
    	 for (let am3=7;am3<8;am3++){
    	  for (let am4=6;am4<7;am4++){
    	   for (let am5=5;am5<6;am5++){
+*/
+   for (let am1=9;am1<10;am1++){
+    for (let am2=7;am2<8;am2++){
+     for (let am3=8;am3<9;am3++){
+      for (let am4=5;am4<6;am4++){
+       for (let am5=6;am5<7;am5++){
    	   	let initialInput=0
         let firstLoop=true;
         if(am1!=am2 && am1!=am3 && am1!=am4 && am1!=am5 && am2!=am3 && am2!=am4 && am2!=am5 && am3!=am4 && am3!=am5 && am4!=am5){
@@ -43,42 +54,48 @@ export const CodeAdvent20190702 = () => {
     let position3=0;
     let position4=0;
     let position5=0;
+    let firstTime=true;
     console.log(maxThrusterSetting);
+    console.log(firstTime);
     maxThrusterSetting=loop(am1, am2, am3, am4, am5);
+    
     function loop(am1, am2, am3, am4, am5, initialInput){
     
-    let finalCode1= calc2(amplifierControllerSoftware1, optInput11, optInput12, position1);
+    let finalCode1= calc2(amplifierControllerSoftware1, optInput11, optInput12, position1, firstTime);
     optInput22=finalCode1[0];
     position1=finalCode1[1];
     amplifierControllerSoftware1=finalCode1[2];
     console.log("finalCode1",finalCode1);
     
     
-    let finalCode2= calc2(amplifierControllerSoftware2, optInput21, optInput22, position2);
+    let finalCode2= calc2(amplifierControllerSoftware2, optInput21, optInput22, position2, firstTime);
     optInput32=finalCode2[0];
     position2=finalCode2[1];
     amplifierControllerSoftware2=finalCode2[2];
     console.log("finalCode2",finalCode2);
     
     
-    let finalCode3= calc2(amplifierControllerSoftware3, optInput31, optInput32, position3);
+    let finalCode3= calc2(amplifierControllerSoftware3, optInput31, optInput32, position3, firstTime);
     optInput42=finalCode3[0];
     position3=finalCode3[1];
     amplifierControllerSoftware3=finalCode3[2];
     console.log("finalCode3",finalCode3);
     
     
-    let finalCode4= calc2(amplifierControllerSoftware4, optInput41, optInput42, position4);
+    let finalCode4= calc2(amplifierControllerSoftware4, optInput41, optInput42, position4, firstTime);
     optInput52=finalCode4[0];
     position4=finalCode4[1];
     amplifierControllerSoftware4=finalCode4[2];
     console.log("finalCode4",finalCode4);
     
     
-    let finalCode5= calc2(amplifierControllerSoftware5, optInput51, optInput52, position5);
+    let finalCode5= calc2(amplifierControllerSoftware5, optInput51, optInput52, position5, firstTime);
     optInput12=finalCode5[0];
     position5=finalCode5[1];
     amplifierControllerSoftware5=finalCode5[2];
+
+    firstTime=false;
+    console.log("firstTime", firstTime);
     console.log("finalCode5",finalCode5);
     
     console.log("amplifierControllerSoftware1", amplifierControllerSoftware1);
@@ -97,12 +114,14 @@ export const CodeAdvent20190702 = () => {
     console.log("optInput52", optInput52);
     console.log("optInput12", optInput12);
 
-    /*if (finalCode5>initialInput){
-    return loop(am1, am2, am3, am4, am5, finalCode5);
+    if ( (amplifierControllerSoftware1[position1]!=99) && (amplifierControllerSoftware2[position2]!=99) && (amplifierControllerSoftware3[position3]!=99) && (amplifierControllerSoftware4[position4]!=99) && (amplifierControllerSoftware5[position5]!=99)){
+    console.log("Condition 1 is triggered");
+    //debugger;
+    loop(am1, am2, am3, am4, am5, initialInput);
     } else {
-    	return initialInput;
+      console.log("Condition 2 is triggered");
     }
-    */
+  
    /* if(finalCode5>maxThrusterSetting){
       maxThrusterSetting=finalCode5;
       maxPhaseSetting.am1=am1;
@@ -139,8 +158,8 @@ export const CodeAdvent20190702 = () => {
 };
 
 
-const calc2 = (input, optCodeInput1, optCodeInput2, position)=> {
-let firstInput=true;
+const calc2 = (input, optCodeInput1, optCodeInput2, position, firstTime)=> {
+let firstInput=firstTime;
 let optCodeOutputArray=[];
 let x=0;
 let currentPosition;
@@ -150,8 +169,9 @@ for (let i=position;i<input.length;i+=x){
 let current=parseInput(input[i]);
 
 if (current.number==99){
+ // debugger;
  //console.log("condition 99 is triggered");
-x=2;
+x=0;
 break;
 } else if(current.one==1 && current.parameterMode1=="position" && current.parameterMode2=="position"){
  //console.log("condition 1 is triggered");
