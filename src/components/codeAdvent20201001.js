a2932baa9f4ed44f9d279e2b58aa605944fad2e2
addania <mirka.schw@gmail.com>
2020-12-10 22:05:07 +0100

    Day102020

--- src/components/codeAdvent20201001.js
+++ src/components/codeAdvent20201001.js
@@ -22,7 +22,69 @@ export const CodeAdvent20201001 = () => {
 
 function fun(input) {
   console.log("input[0]", input[0])
-  for (let i = 0; i < input.length; i++) {}
+  const sortedList = input.sort(function(a, b) {
+    return a - b
+  })
+  console.log("sortedList", sortedList)
+  let base = 0
+  let count1 = 0
+  let count2 = 0
+  let count3 = 0
+  let deviceOutput = sortedList[sortedList.length - 1] + 3
+  console.log("deviceOutput", deviceOutput)
+  let data = []
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] - base <= 3 && input[i] - base > 0) {
+      let difference = input[i] - base
+      if (difference === 1) {
+        count1 += 1
+        data.push({
+          adapter: input[i],
+          difference: difference,
+          currentOutput: input[i],
+          count1: count1,
+          count2: count2,
+          count3: count3,
+        })
+      } else if (difference === 2) {
+        count2 += 1
+        data.push({
+          adapter: input[i],
+          difference: difference,
+          currentOutput: input[i],
+          count1: count1,
+          count2: count2,
+          count3: count3,
+        })
+      } else if (difference === 3) {
+        count3 += 1
+        data.push({
+          adapter: input[i],
+          difference: difference,
+          currentOutput: input[i],
+          count1: count1,
+          count2: count2,
+          count3: count3,
+        })
+      }
+      base = input[i]
+    }
+  }
 
-  return 1
+  base = base + 3
+  count3 += 1
+  data.push({
+    adapter: base + 3,
+    difference: 3,
+    currentOutput: base + 3,
+    count1: count1,
+    count2: count2,
+    count3: count3,
+  })
+  console.log("data", data)
+  console.log("count1", count1)
+  console.log("count2", count2)
+  console.log("count3", count3)
+  console.log("base", base)
+  return count1 * count3
 }
--- src/components/codeAdvent20201002.js
+++ src/components/codeAdvent20201002.js
@@ -6,7 +6,12 @@ export const CodeAdvent20201002 = () => {
   const [x] = useState(data)
   const [total, setTotal] = useState()
   function handleClick() {
-    let result = fun(x)
+    const sortedList = x.sort(function(a, b) {
+      return a - b
+    })
+    let prefix = [0]
+    let index = 0
+    let result = fun(sortedList, prefix, index)
     setTotal(result)
   }
   return (
@@ -20,9 +25,141 @@ export const CodeAdvent20201002 = () => {
   )
 }
 
-function fun(input) {
-  console.log("input[0]", input[0])
-  for (let i = 0; i < input.length; i++) {}
+function fun(sortedList, prefix, index) {
+  let final = [[0]]
+  for (let i = index; i < sortedList.length; i++) {
+    let additions = []
+    for (let j = 0; j < final.length; j++) {
+      if (final[j][i] === 0) {
+        let diff1 = sortedList[i] - 0
+        let diff2 = sortedList[i + 1] - 0
+        let diff3 = sortedList[i + 2] - 0
+        //     console.log("diff1", diff1)
+        //    console.log("diff2", diff2)
+        //    console.log("diff3", diff3)
+        if (diff1 === 3) {
+          final[j].push(sortedList[i])
+          //     console.log("condition 1")
+          //     console.log("final", final)
+        } else if (diff1 === 2 && diff2 > 3) {
+          final[j].push(sortedList[i])
+          //    console.log("condition 2")
+          //    console.log("final", final)
+        } else if (diff1 === 2 && diff2 === 3) {
+          //    console.log("final[j]", final[j])
+          //    console.log("sortedList[i]", sortedList[i])
+          final.push([...final[j]])
+          final[final.length - 1].push(sortedList[i + 1])
+          final[j].push(sortedList[i])
+          j = j + 1
+          //   console.log("condition 3")
+          //   console.log("final", final)
+        } else if (diff1 === 1 && diff2 > 3) {
+          final[j].push(sortedList[i])
+          //    console.log("condition 4")
+          //   console.log("final", final)
+        } else if (
+          diff1 === 1 &&
+          diff2 <= 3 &&
+          (diff3 > 3 || Number.isNaN(diff3))
+        ) {
+          //   console.log("final[j]", final[j])
+          //   console.log("sortedList[i]", sortedList[i])
+          final.push([...final[j]])
+          final[final.length - 1].push(sortedList[i + 1])
+          final[j].push(sortedList[i])
+          j = j + 1
+          //   console.log("condition 5")
+          //   console.log("final", final)
+        } else if (diff1 === 1 && diff2 === 2 && diff3 === 3) {
+          //  console.log("final[j]", final[j])
+          //  console.log("sortedList[i]", sortedList[i])
+          final.push([...final[j]])
+          final[final.length - 1].push(sortedList[i + 1])
+          final.push([...final[j]])
+          final[final.length - 1].push(sortedList[i + 2])
+          final[j].push(sortedList[i])
+          j = j + 2
+          //   console.log("condition 6")
+          //  console.log("final", final)
+        }
+      } else {
+        let lastNumber = final[j][i]
+        //  console.log("lastNumber", lastNumber)
+        let position = sortedList.indexOf(lastNumber)
+        //  console.log("position", position)
+        let diff1 = sortedList[position + 1] - sortedList[position]
+        let diff2 = sortedList[position + 2] - sortedList[position]
+        let diff3 = sortedList[position + 3] - sortedList[position]
+        //  console.log("diff1", diff1)
+        //  console.log("diff2", diff2)
+        //  console.log("diff3", diff3)
+        //  console.log("final", final)
 
-  return 1
+        if (diff1 === 3) {
+          final[j].push(sortedList[position + 1])
+          //    console.log("conditionnnnn 1")
+          //    console.log("final", final)
+        } else if (diff1 === 2 && diff2 > 3) {
+          final[j].push(sortedList[position + 1])
+          //    console.log("conditionnnnn 2")
+          //    console.log("final", final)
+        } else if (diff1 === 2 && diff2 === 3) {
+          //    console.log("final[j]", final[j])
+          //    console.log("sortedList[i]", sortedList[i])
+          let x = final[j].slice()
+          x.push(sortedList[position + 2])
+          additions.push(x)
+          //    console.log("additions", additions)
+          final[j].push(sortedList[position + 1])
+          //    console.log("conditionnnnn 3")
+          //    console.log("final", final)
+        } else if (diff1 === 1 && diff2 > 3) {
+          final[j].push(sortedList[position + 1])
+          //    console.log("conditionnnnn 4")
+          //    console.log("final", final)
+        } else if (
+          diff1 === 1 &&
+          diff2 <= 3 &&
+          (diff3 > 3 || Number.isNaN(diff3))
+        ) {
+          //    console.log("final[j]", final[j])
+          //    console.log("sortedList[i]", sortedList[i])
+          let x = final[j].slice()
+          //    console.log("x", x)
+          x.push(sortedList[position + 2])
+          //debugger
+          additions.push(x)
+          //    console.log("additions", additions)
+
+          final[j].push(sortedList[position + 1])
+          //    console.log("conditionnnnnnnnnnn 5")
+          //    console.log("final", final)
+          //debugger
+        } else if (diff1 === 1 && diff2 === 2 && diff3 === 3) {
+          //  console.log("final[j]", final[j])
+          //  console.log("sortedList[i]", sortedList[i])
+
+          let x = final[j].slice()
+          x.push(sortedList[position + 2])
+          additions.push(x)
+
+          let y = final[j].slice()
+          y.push(sortedList[position + 3])
+          additions.push(y)
+          final[j].push(sortedList[position + 1])
+          //   console.log("conditionnnnnn 6")
+          //  console.log("final", final)
+        }
+      }
+    }
+    if (additions.length > 0) {
+      for (let z = 0; z < additions.length; z++) {
+        final.push(additions[z])
+      }
+    }
+    // console.log("final", final)
+  }
+
+  return final.length
 }
