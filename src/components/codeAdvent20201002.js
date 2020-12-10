2e1792bdcbc41b5feffa01bf84c32136dcf8b8ed
addania <mirka.schw@gmail.com>
2020-12-10 23:41:18 +0100

    Day10 2020 simplification

--- src/components/codeAdvent20201002.js
+++ src/components/codeAdvent20201002.js
@@ -30,72 +30,63 @@ function fun(sortedList, prefix, index) {
   for (let i = index; i < sortedList.length; i++) {
     let additions = []
     for (let j = 0; j < final.length; j++) {
-      if (final[j][i] === 0) {
-        let diff1 = sortedList[i] - 0
-        let diff2 = sortedList[i + 1] - 0
-        let diff3 = sortedList[i + 2] - 0
+      if (final[j][0] === 0) {
+        let diff1 = sortedList[0]
+        let diff2 = sortedList[1]
+        let diff3 = sortedList[2]
         if (diff1 === 3) {
-          final[j].push(sortedList[i])
+          final[j] = [sortedList[0]]
         } else if (diff1 === 2 && diff2 > 3) {
-          final[j].push(sortedList[i])
+          final[j] = [sortedList[0]]
         } else if (diff1 === 2 && diff2 === 3) {
-          final.push([...final[j]])
-          final[final.length - 1].push(sortedList[i + 1])
-          final[j].push(sortedList[i])
+          final.push([sortedList[1]])
+          final[j] = [sortedList[0]]
+
           j = j + 1
         } else if (diff1 === 1 && diff2 > 3) {
-          final[j].push(sortedList[i])
+          final[j] = [sortedList[0]]
         } else if (
           diff1 === 1 &&
           diff2 <= 3 &&
           (diff3 > 3 || Number.isNaN(diff3))
         ) {
-          final.push([...final[j]])
-          final[final.length - 1].push(sortedList[i + 1])
-          final[j].push(sortedList[i])
+          final.push([sortedList[1]])
+          final[j] = [sortedList[0]]
+
           j = j + 1
         } else if (diff1 === 1 && diff2 === 2 && diff3 === 3) {
-          final.push([...final[j]])
-          final[final.length - 1].push(sortedList[i + 1])
-          final.push([...final[j]])
-          final[final.length - 1].push(sortedList[i + 2])
-          final[j].push(sortedList[i])
+          final.push([sortedList[1]])
+          final[j] = [sortedList[0]]
+          final.push([sortedList[2]])
+
           j = j + 2
         }
       } else {
-        let lastNumber = final[j][i]
+        let lastNumber = final[j][0]
         let position = sortedList.indexOf(lastNumber)
         let diff1 = sortedList[position + 1] - sortedList[position]
         let diff2 = sortedList[position + 2] - sortedList[position]
         let diff3 = sortedList[position + 3] - sortedList[position]
         if (diff1 === 3) {
-          final[j].push(sortedList[position + 1])
+          final[j] = [sortedList[position + 1]]
         } else if (diff1 === 2 && diff2 > 3) {
-          final[j].push(sortedList[position + 1])
+          final[j] = [sortedList[position + 1]]
         } else if (diff1 === 2 && diff2 === 3) {
-          let x = final[j].slice()
-          x.push(sortedList[position + 2])
-          additions.push(x)
-          final[j].push(sortedList[position + 1])
+          additions.push([sortedList[position + 2]])
+          final[j] = [sortedList[position + 1]]
         } else if (diff1 === 1 && diff2 > 3) {
-          final[j].push(sortedList[position + 1])
+          final[j] = [sortedList[position + 1]]
         } else if (
           diff1 === 1 &&
           diff2 <= 3 &&
           (diff3 > 3 || Number.isNaN(diff3))
         ) {
-          let x = final[j].slice()
-          x.push(sortedList[position + 2])
-          additions.push(x)
-          final[j].push(sortedList[position + 1])
+          additions.push([sortedList[position + 2]])
+          final[j] = [sortedList[position + 1]]
         } else if (diff1 === 1 && diff2 === 2 && diff3 === 3) {
-          let x = final[j].slice()
-          x.push(sortedList[position + 2])
-          additions.push(x)
-          let y = final[j].slice()
-          y.push(sortedList[position + 3])
-          additions.push(y)
-          final[j].push(sortedList[position + 1])
+          additions.push([sortedList[position + 2]])
+          final[j] = [sortedList[position + 1]]
+          additions.push([sortedList[position + 3]])
         }
       }
     }
